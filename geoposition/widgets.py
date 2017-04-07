b12566a86474c565b82e777a0b8dcf693d104711
Tomeu Canyelles Escarrer <bcanyelles@apsl.net>
2017-04-07 12:01:24 +0200

    Update widget to pass extra context on render template

--- geoposition/widgets.py
+++ geoposition/widgets.py
@@ -4,6 +4,7 @@ import json
 from django import forms
 from django.template.loader import render_to_string
 from django.utils import six
+from django.utils.safestring import mark_safe
 from django.utils.translation import ugettext_lazy as _
 from .conf import settings
 
@@ -23,22 +24,46 @@ class GeopositionWidget(forms.MultiWidget):
             return [value.latitude, value.longitude]
         return [None, None]
 
-    def format_output(self, rendered_widgets):
-        return render_to_string('geoposition/widgets/geoposition.html', {
-            'latitude': {
-                'html': rendered_widgets[0],
-                'label': _("latitude"),
-            },
-            'longitude': {
-                'html': rendered_widgets[1],
-                'label': _("longitude"),
-            },
+    def render(self, name, value, attrs=None):
+        if self.is_localized:
+            for widget in self.widgets:
+                widget.is_localized = self.is_localized
+        # value is a list of values, each corresponding to a widget
+        # in self.widgets.
+        if not isinstance(value, list):
+            value = self.decompress(value)
+
+        final_attrs = self.build_attrs(attrs)
+        id_ = final_attrs.get('id')
+
+        context = {'id': id_, 'name': name, 'fields': []}
+        for i, widget in enumerate(self.widgets):
+            try:
+                widget_value = value[i]
+            except IndexError:
+                widget_value = None
+            if id_:
+                final_attrs = dict(final_attrs, id='%s_%s' % (id_, i))
+            context['fields'].append(widget.render(name + '_%s' % i, widget_value, final_attrs))
+        context.update(self.get_extra_context())
+
+        return mark_safe(self.format_output(context))
+
+    def get_extra_context(self):
+        return {
             'config': {
                 'map_widget_height': settings.MAP_WIDGET_HEIGHT or 500,
                 'map_options': json.dumps(settings.MAP_OPTIONS),
                 'marker_options': json.dumps(settings.MARKER_OPTIONS),
             }
+        }
+
+    def format_output(self, context):
+        context.update({
+            'latitude': {'html': context['fields'][0], 'label': _("latitude")},
+            'longitude': {'html': context['fields'][1], 'label': _("longitude")},
         })
+        return render_to_string('geoposition/widgets/geoposition.html', context)
 
     class Media:
         js = (
